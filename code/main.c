ef1f0a40356d00eab76b9200ec970a743725fdec
wjorden <wjorden@krampus.tech>
2024-09-29 09:02:20 -0400

    changed includes and added SREG ref

--- code/main.c
+++ code/main.c
@@ -1,7 +1,14 @@
+#include <stdint.h>
+#include "arduino/interrupts.h"
 #include <avr/io.h>
 #include <util/delay.h>
 
 int main(void){
+    uint8_t sreg = SREG;
+    /* *
+    * Since we need the reset pin, PC6 is unavailable. Reset also means we need the
+    * SREG reference to handle the interrupt.
+    * */
     DDRB |= (1 << 5);
     while(1){
         PORTB |= (1 << 5);
